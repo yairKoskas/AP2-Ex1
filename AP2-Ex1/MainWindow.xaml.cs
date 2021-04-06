721b098e79cd128d74649c4bc7922b1da8e4b89b
yairkoskas <yairi2003@gmail.com>
2021-04-06 20:35:15 +0300

    changed toolbar colors

--- AP2-Ex1/MainWindow.xaml.cs
+++ AP2-Ex1/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AP2_Ex1
 {
@@ -25,7 +13,7 @@ namespace AP2_Ex1
             InitializeComponent();
         }
 
-        private void button_click(object sender, RoutedEventArgs e)
+        private void load_csv(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.DefaultExt = ".csv";
