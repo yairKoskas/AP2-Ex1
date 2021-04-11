538f1271adb3e17a7532cce3f482cb102efdd358
ariel <ariel@DESKTOP-TBI0PE8>
2021-04-11 21:18:39 +0300

    continued implementation of fileloader model

--- AP2-Ex1/FileLoaderModel.cs
+++ AP2-Ex1/FileLoaderModel.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace AP2_Ex1
 {
     class FileLoaderModel : IFileLoaderModel
     {
         List<String> CSVLines;
-        string XMLContent;
+        String XMLContent;
         public event IFileLoaderModel.Notifier NotifyCSVChanged;
         public event IFileLoaderModel.Notifier NotifyXMLChanged;
 
-        public
+        public FileLoaderModel()
+        {
+            CSVLines = new List<String>();
+            XMLContent = "";
+        }
         public void LoadCSV()
         {
-            throw new NotImplementedException();
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "CSV Files (*.csv)|*.csv";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                var path = dlg.FileName;
+                var content = File.ReadAllLines(path);
+                CSVLines = new List<string>(content);
+            }
+            NotifyCSVChanged();
         }
 
         public void LoadXML()
         {
             throw new NotImplementedException();
+        }
+
+        public string getCSVLine(int x)
+        {
+            return CSVLines[x];
         }
     }
 }
