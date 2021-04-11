7bc7cf08be990182afd713515de540f0b227652a
ariel <ariel@DESKTOP-TBI0PE8>
2021-04-11 20:56:10 +0300

    started loading file loading architecture(model interface and implementation)

---
+++ AP2-Ex1/FileLoaderModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP2_Ex1
+{
+    class FileLoaderModel : IFileLoaderModel
+    {
+        List<String> CSVLines;
+        string XMLContent;
+        public event IFileLoaderModel.Notifier NotifyCSVChanged;
+        public event IFileLoaderModel.Notifier NotifyXMLChanged;
+
+        public
+        public void LoadCSV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadXML()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AP2-Ex1/IFileLoaderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace AP2_Ex1
+{
+    interface IFileLoaderModel
+    {
+        void LoadCSV();
+        void LoadXML();
+        public delegate void Notifier();
+        public event Notifier NotifyCSVChanged, NotifyXMLChanged;
+    }
+}
