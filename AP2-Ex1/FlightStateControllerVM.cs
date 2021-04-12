2a6da93f6ae32e2c2fcc75b8e41ebb31956a5a31
itay Gradenwits <itay.gradenwits@live.biu.ac.il>
2021-04-12 11:52:21 +0300

    implement FlightStateControllerVM.

---
+++ AP2-Ex1/FlightStateControllerVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP2_Ex1
+{
+    class FlightStateControllerVM
+    {
+        private IFlightStateControllerModel flightStateController;
+
+        private int currentIndexOfLine =0;
+        private int numberOfLines =0;
+
+        public FlightStateControllerVM(FlightStateControllerModel model)
+        {
+            this.flightStateController = model;
+            model.notifyCurrentIndexChanged += delegate ()
+            {
+                currentIndexOfLine = model.CurrentIndexOfLine;
+            };
+
+            model.notifyNumberOfLinesChanged += delegate ()
+            {
+                numberOfLines = model.NumberOfCSVLines;
+            };
+        }
+        public void changeIndex()
+        {
+            flightStateController.changeIndexOfLine(currentIndexOfLine);
+        }
+
+    }
+}
