62566478024288781872c795fc94ae9804bfa187
itay Gradenwits <itay.gradenwits@live.biu.ac.il>
2021-04-12 11:03:16 +0300

    implement the FlightStateControllerModel

---
+++ AP2-Ex1/FlightStateControllerModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP2_Ex1
+{
+    class FlightStateControllerModel : IFlightStateControllerModel
+    {
+        public event Notifier notifyNumberOfLinesChanged;
+        public event Notifier notifyCurrentIndexChanged;
+        private IFileLoaderModel fileLoader;
+        private int currentIndexOfLine = 0;
+        private int numberOfCSVLines = 0;
+        public FlightStateControllerModel(IFileLoaderModel fileLoader)
+        {
+            this.fileLoader = fileLoader;
+            fileLoader.NotifyCSVChanged += delegate ()
+            {
+                this.NumberOfCSVLines = fileLoader.GetNumOfCSVLines();
+            };
+        }
+        public int NumberOfCSVLines {
+            get { return this.numberOfCSVLines; } 
+            set
+            {
+                this.numberOfCSVLines = value;
+                notifyNumberOfLinesChanged();
+            } 
+        }
+        public int CurrentIndexOfLine {
+            get { return this.currentIndexOfLine; }
+            set 
+            {
+                this.currentIndexOfLine = value;
+                notifyCurrentIndexChanged();
+            } 
+        }
+
+        public void changeIndexOfLine(int x)
+        {
+            this.currentIndexOfLine = x;
+        }
+
+        public string getLine()
+        {
+            return this.fileLoader.GetCSVLine(currentIndexOfLine);
+        }
+    }
+}
---
+++ AP2-Ex1/IFlightStateControllerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP2_Ex1
+{
+    interface IFlightStateControllerModel
+    {
+        public String getLine();
+
+        public int NumberOfCSVLines { get; set; }
+        public void changeIndexOfLine(int x);
+        public int CurrentIndexOfLine { get; set; }
+
+        public event Notifier notifyNumberOfLinesChanged;
+        public event Notifier notifyCurrentIndexChanged;
+
+    }
+}
