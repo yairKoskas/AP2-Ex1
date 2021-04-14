83a53c5ba1eab1e3293c82fa2fe5bc9323875f63
itay Gradenwits <itay.gradenwits@live.biu.ac.il>
2021-04-14 14:54:01 +0300

    add file

---
+++ AP2-Ex1/controls/SimulationSpeedController.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AP2_Ex1.controls
+{
+    /// <summary>
+    /// Interaction logic for SimulationSpeedController.xaml
+    /// </summary>
+    public partial class SimulationSpeedController : UserControl
+    {
+        public SimulationSpeedController()
+        {
+            InitializeComponent();
+        }
+    }
+}
